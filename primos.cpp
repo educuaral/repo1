3c7f87c85ed4c842fb4cbd9729b1a68210d883cb
educuaral <30728989+educuaral@users.noreply.github.com>
2017-08-05 11:25:17 -0500

    Números primos entre 500 y 1400

---
+++ primos.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+  for (int i=2; i<=10; i++){
+    int c=0;
+      for(int k=1;k<=10;k++){
+        if(i%k==0)
+           c++;}
+        if(c==2){
+            cout<< i <<endl;}
+            }
+
+}
+
+
