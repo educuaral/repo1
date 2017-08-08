c6257f02d034ae297b6770fb817a8346e52c08a5
educuaral <30728989+educuaral@users.noreply.github.com>
2017-08-07 19:11:59 -0500

    coreccion (500-1400)

--- primos.cpp
+++ primos.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(void)
 {
-  for (int i=2; i<=10; i++){
+  for (int i=500; i<=1400; i++){
     int c=0;
-      for(int k=1;k<=10;k++){
+      for(int k=1;k<=1400;k++){
         if(i%k==0)
            c++;}
         if(c==2){
